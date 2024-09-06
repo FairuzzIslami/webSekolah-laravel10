6be15fcd83d2c77aa0cddfce5ff1bdc0af165c61
Fairuz <firdauz2007123@gmail.com>
2024-09-06 18:30:38 +0700

    tambahkan bagian berita

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,41 +56,98 @@
             </section>
 
             <section id="card" style="margin-top:-40px">
-                <div class="container">
-                    <div class="row">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-3">
+                                <div class="bg-white rounded-3 p-3 d-flex justify-content-between align-items-center ">
+                                    <h4>Pendidikan berkualitas</h4>
+                                    <img src="{{asset('asset/icon/ic-book.png')}}" height="100px" alt="">
+                                </div>
+                            </div>
                         <div class="col-lg-3">
                             <div class="bg-white rounded-3 p-3 d-flex justify-content-between align-items-center ">
-                                <h4>Pendidikan berkualitas</h4>
-                                <img src="{{asset('asset/icon/ic-book.png')}}" height="100px" alt="">
+                                <h4>Pendidikan berahlak</h4>
+                                <img src="{{asset('asset/icon/ic-globe.png')}}" height="100px" alt="">
                             </div>
                         </div>
-                    <div class="col-lg-3">
-                        <div class="bg-white rounded-3 p-3 d-flex justify-content-between align-items-center ">
-                            <h4>Pendidikan berahlak</h4>
-                            <img src="{{asset('asset/icon/ic-globe.png')}}" height="100px" alt="">
+                        <div class="col-lg-3">
+                            <div class="bg-white rounded-3 p-3 d-flex justify-content-between align-items-center ">
+                                <h4>Pendidikan muamalah</h4>
+                                <img src="{{asset('asset/icon/ic-neraca.png')}}" height="100px" alt="">
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="bg-white rounded-3 p-3 d-flex justify-content-between align-items-center ">
+                                <h4>Pendidikan Teknologi</h4>
+                                <img src="{{asset('asset/icon/ic-komputer.png')}}" height="100px" alt="">
+                            </div>
                         </div>
                     </div>
-                    <div class="col-lg-3">
-                        <div class="bg-white rounded-3 p-3 d-flex justify-content-between align-items-center ">
-                            <h4>Pendidikan muamalah</h4>
-                            <img src="{{asset('asset/icon/ic-neraca.png')}}" height="100px" alt="">
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="bg-white rounded-3 p-3 d-flex justify-content-between align-items-center ">
-                            <h4>Pendidikan Teknologi</h4>
-                            <img src="{{asset('asset/icon/ic-komputer.png')}}" height="100px" alt="">
-                        </div>
-                    </div>
-                </div>
-            </div>
-                
-                    
-                    </div>
-                </div>
-                
-                
+                </div>      
             </section>
+
+            {{-- berita --}}
+            <section id="berita">
+                <div class="container py-5 ">
+                    <div class="header-berita text-center align-item-center">
+                        <h2 class="fw-bold">Berita kegiatan Pondok pesantren</h2>
+                    </div>
+
+                        <div class="row py-5">
+                            <div class="col-lg-4">
+                                    <div class="card border-0">
+                                        <img src="{{asset('asset/image/il-berita-01.png')}}" class="img-fluid mb-3" alt="">
+                                    <div class="konten-berita">
+                                        <p class="py-3 ">9 September 2024</p>
+                                        <h4 class="fw-bold">Kegiatan Ceramah Bulanan Pesantren</h4>
+                                        <p class="text-secondary">#beritaPesantren</p>
+                                        <a href=""  style="text-decoration: none" class="text-danger fw-bold fs-5">Selengkapnya</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="card border-0">
+                                    <img src="{{asset('asset/image/il-berita-03.png')}}" class="img-fluid mb-3" alt="">
+                                <div class="konten-berita">
+                                    <p class="py-3 ">12 Agustus 2024</p>
+                                    <h4 class="fw-bold">Saling Menghargai antar Sesama santri Lain</h4>
+                                    <p class="text-secondary">#beritaPesantren</p>
+                                    <a href=""  style="text-decoration: none" class="text-danger fw-bold fs-5">Selengkapnya</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="card border-0">
+                                <img src="{{asset('asset/image/il-berita-02.png')}}" class="img-fluid mb-3" alt="">
+                                <div class="konten-berita">
+                                    <p class="py-3 ">24 Maret 2024</p>
+                                    <h4 class="fw-bold">Pembiasaan tadarus Secara Bersama-sama</h4>
+                                    <p class="text-secondary">#beritaPesantren</p>
+                                    <a href=""  style="text-decoration: none" class="text-danger fw-bold fs-5">Selengkapnya</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="footer-berita text-center py-5">
+                        <a href="" class=" btn btn-outline-danger">Berita lainnya</a>
+                    </div>
+                </div>
+            </section>
+            {{-- berita akhir --}}
         </body>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <script>
+            const navbar = document.querySelector(".fixed-top");
+            window.onscroll = () =>{
+                if (window.scrollY > 100) {
+                    navbar.classList.add("scroll-nav-active")
+                    navbar.classList.add("text-nav-active")
+                    navbar.classList.remove("navbar-dark")
+                }else{
+                    navbar.classList.remove("navbar-dark")
+                    navbar.classList.add("scroll-nav-active")
+                }
+            }
+        </script>
     </html>
