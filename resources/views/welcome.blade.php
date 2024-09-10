db5d59d161a058a275b1dbeb6460df7d7c40821b
Fairuz <firdauz2007123@gmail.com>
2024-09-10 22:07:59 +0700

    add section foto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -195,6 +195,36 @@
                 </div>
             </section>
             {{-- vidio akhir --}}
+            
+            {{-- foto --}}
+            <section id="foto" class="section-foto paralax">
+                <div class="container">
+                    <div class="d-flex justify-content-between align-items-center mb-5">
+                        <div class="d-flex align-items-center">
+                            <div class="stripe-putih me-2"></div>
+                            <h5 class="fw-bold text-white">Foto Kegiatan</h5>
+                        </div>
+                        <div>
+                            <a href="" class="btn btn-outline-white">Foto lainnya</a>
+                        </div>
+                    </div>
+                    <div class="row py-5">
+                        <div class="col-lg-3 col-md-6 col-6">
+                            <img src="{{asset("asset/image/il-photo-01.png")}}" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-6">
+                            <img src="{{asset("asset/image/il-photo-02.png")}}" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-6">
+                            <img src="{{asset("asset/image/il-photo-03.png")}}" alt="" class="img-fluid">
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-6">
+                            <img src="{{asset("asset/image/il-photo-04.png")}}" alt="" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+            </section> 
+            {{-- foto akhir --}}
         </body>
         
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
